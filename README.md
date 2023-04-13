f273efb3aea043f47104dd7cdf66dd728e4a609c
Abirami Palanikumar <abiramipalanikumar179@gmail.com>
2023-04-13 17:03:28 +0530

    Intial code setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# EssayBot
-A Essay Summariser 
+# Essay Bot
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+import App from "./App";
+import "./index.css";
+
+import "primereact/resources/themes/lara-light-blue/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css"; // flex
+import PrimeReact from "primereact/api";
+PrimeReact.ripple = true;
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
