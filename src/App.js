d225def96206b5492a18769e59d77b4f8a34e15f
Gokul Ganesh I <igokulganesh@gmail.com>
2023-04-14 23:48:13 +0530

    Added Webpack Module bundler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import './apicall.js';
-import axios from 'axios';
+import React from 'react';
+import axios from "axios";
+import "./App.css";
+
 function apiCall() {
-  const openaiApiUrl = 'https://api.openai.com/v1/completions';
+  const openaiApiUrl = "https://api.openai.com/v1/completions";
 
   const headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     Authorization: `Bearer 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`,
   };
 
@@ -16,22 +17,28 @@ function apiCall() {
     temperature: 1,
   };
 
-  axios.post(openaiApiUrl, data, { headers })
-    .then(response => {
+  axios
+    .post(openaiApiUrl, data, { headers })
+    .then((response) => {
       console.log(response.data.choices[0]);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
     });
 }
 
 function App() {
-
   return (
     <div className="App">
       <h1>Welcome To EssayBot</h1>
-      <label style={{ color: "red", fontSize: "18px", textAlign: 'left' }}><b>Enter Your Topic</b></label>
-      <input id="inputtext" type="text" style={{ border: "1px solid black", padding: "5px", fontSize: "16px" }} />
+      <label style={{ color: "red", fontSize: "18px", textAlign: "left" }}>
+        <b>Enter Your Topic</b>
+      </label>
+      <input
+        id="inputtext"
+        type="text"
+        style={{ border: "1px solid black", padding: "5px", fontSize: "16px" }}
+      />
       <button onClick={() => apiCall()}>Create Essay</button>
     </div>
   );
