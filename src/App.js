fe1d63e9b074e1098ef2f3b6d28258a607fb7562
Abirami Palanikumar <abiramipalanikumar179@gmail.com>
2023-04-14 21:02:25 +0530

    Js And Html Code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import './apicall.js';
+import axios from 'axios';
+function apiCall() {
+  const openaiApiUrl = 'https://api.openai.com/v1/completions';
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer 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`,
+  };
+
+  const data = {
+    model: "text-davinci-003",
+    prompt: "sachintendulkar",
+    max_tokens: 400,
+    temperature: 1,
+  };
+
+  axios.post(openaiApiUrl, data, { headers })
+    .then(response => {
+      console.log(response.data.choices[0]);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Hello Abi</h1>
+      <h1>Welcome To EssayBot</h1>
+      <label style={{ color: "red", fontSize: "18px", textAlign: 'left' }}><b>Enter Your Topic</b></label>
+      <input id="inputtext" type="text" style={{ border: "1px solid black", padding: "5px", fontSize: "16px" }} />
+      <button onClick={() => apiCall()}>Create Essay</button>
     </div>
   );
 }
